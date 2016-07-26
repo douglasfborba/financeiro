8898711875b67b004b9bf950469f568026171f0f
Douglas Borba <douglasfborba@gmail.com>
2016-07-26 11:11:38 -0300

    Atualização da página usuario.xhtml, da classe UsuarioBean e do arquivo faces-config.

--- src/main/java/br/com/financeiro/web/UsuarioBean.java
+++ src/main/java/br/com/financeiro/web/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.financeiro.web;
 
+import java.util.List;
+
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
@@ -11,10 +13,15 @@ import br.com.financeiro.usuario.UsuarioRN;
 @ManagedBean(name = "usuarioBean")
 @RequestScoped
 public class UsuarioBean {
-	private Usuario usuario;
+	private Usuario usuario = new Usuario();
 	private String confirmarSenha;
+	private List<Usuario> lista;
+	private String destinoSalvar;
 
 	public String novo() {
+		this.destinoSalvar = "usuarioSucesso";
+		this.usuario = new Usuario();
+		this.usuario.setAtivo(true);
 		return "usuario";
 	}
 
@@ -25,18 +32,35 @@ public class UsuarioBean {
 			FacesMessage facesMessage = new FacesMessage("A senha não foi confirmada corretamente");
 			context.addMessage(null, facesMessage);
 			return null;
-		} else {
-			UsuarioRN usuarioRN = new UsuarioRN();
-			usuarioRN.salvar(this.usuario);
-			return "usuarioSucesso";
 		}
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return this.destinoSalvar;
 	}
 
-	public UsuarioBean() {
-		this.usuario = new Usuario();
-		this.usuario.setAtivo(true);
+	public String editar() {
+		this.confirmarSenha = this.usuario.getSenha();
+		return "/publico/usuario";
 	}
-	
+
+	public String ativar() {
+		if (this.usuario.isAtivo()) {
+			this.usuario.setAtivo(false);
+		} else {
+			this.usuario.setAtivo(true);
+		}
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.salvar(this.usuario);
+		return null;
+	}
+
+	public String excluir() {
+		UsuarioRN usuarioRN = new UsuarioRN();
+		usuarioRN.excluir(this.usuario);
+		this.lista = null;
+		return null;
+	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
@@ -51,5 +75,21 @@ public class UsuarioBean {
 
 	public void setConfirmarSenha(String confirmarSenha) {
 		this.confirmarSenha = confirmarSenha;
+	}
+
+	public List<Usuario> getLista() {
+		if (this.lista == null) {
+			UsuarioRN usuarioRN = new UsuarioRN();
+			this.lista = usuarioRN.listar();
+		}
+		return this.lista;
+	}
+
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
 	}
 }
