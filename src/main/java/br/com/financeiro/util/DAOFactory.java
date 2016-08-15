88cbe91895af760dbfbbfbc6305e29bb2660fca0
Douglas Borba <douglasfborba@gmail.com>
2016-08-14 21:49:12 -0300

    Criação da camada de acesso a dados do cadastro de categorias.

--- src/main/java/br/com/financeiro/util/DAOFactory.java
+++ src/main/java/br/com/financeiro/util/DAOFactory.java
@@ -1,5 +1,7 @@
 package br.com.financeiro.util;
 
+import br.com.financeiro.categoria.CategoriaDAO;
+import br.com.financeiro.categoria.CategoriaDAOHibernate;
 import br.com.financeiro.conta.ContaDAO;
 import br.com.financeiro.conta.ContaDAOHibernate;
 import br.com.financeiro.usuario.UsuarioDAO;
@@ -16,5 +18,11 @@ public class DAOFactory {
 		ContaDAOHibernate contaDAO = new ContaDAOHibernate();
 		contaDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
 		return contaDAO;
+	}
+
+	public static CategoriaDAO criarCategoriaDAO() {
+		CategoriaDAOHibernate categoriaDAO = new CategoriaDAOHibernate();
+		categoriaDAO.setSession(HibernateUtil.getSessionFactory().getCurrentSession());
+		return categoriaDAO;
 	}
 }
