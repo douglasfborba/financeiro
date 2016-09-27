8453597152678ee51666ebc8f3cfbc023881e67c
Douglas Borba <douglasfborba@gmail.com>
2016-09-27 19:51:57 -0300

    Inclusão de novos métodos para leitura dos arquivos de configuração.

--- src/main/java/br/com/financeiro/web/util/MensagemUtil.java
+++ src/main/java/br/com/financeiro/web/util/MensagemUtil.java
@@ -1,12 +1,14 @@
 package br.com.financeiro.web.util;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import javax.faces.context.FacesContext;
 
 public class MensagemUtil {
-	
+	private static final String MENSAGENS_IDIOMAS = "mensagens";
+
 	public static String getMensagem(String propriedade) {
 		FacesContext context = FacesContext.getCurrentInstance();
 		ResourceBundle bundle = context.getApplication().getResourceBundle(context, "msg");
@@ -17,5 +19,16 @@ public class MensagemUtil {
 		String mensagem = getMensagem(propriedade);
 		MessageFormat formatter = new MessageFormat(mensagem);
 		return formatter.format(paramentros);
+	}
+
+	public static String getMensagem(Locale locale, String propriedade) {
+		ResourceBundle bundle = ResourceBundle.getBundle(MensagemUtil.MENSAGENS_IDIOMAS, locale);
+		return bundle.getString(propriedade);
+	}
+
+	public static String getMensagem(Locale locale, String propriedade, Object... parametros) {
+		String mensagem = MensagemUtil.getMensagem(locale, propriedade);
+		MessageFormat formatter = new MessageFormat(mensagem);
+		return formatter.format(parametros);
 	}
 }
